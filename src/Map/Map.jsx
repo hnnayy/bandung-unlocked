68d79879c2de767c94f5b924e29c45addaef1555
Maulana Hafiz <manl.hzzz01@gmail.com>
2024-06-30 17:02:33 +0700

    change maps api, make list bar more greatfully

--- src/Map/Map.jsx
+++ src/Map/Map.jsx
@@ -3,29 +3,27 @@ import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab/Rating';
-
-import useStyles from './styles';
+import { MapContainer,TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import useStyles from './styles.js';
+import './style.css';
 
 const Map = ({ coords, places, setCoords, setBounds, setChildClicked, weatherData }) => {
   
   const classes = useStyles();
   const isMobile = useMediaQuery('(min-width:600px)');
 
-  const coordinates = coords || { lat: -6.9175, lng: 107.6191 }; // Default coordinates to Bandung
+  const coord = [-6.9175, 107.6191];
 
   return (
-    <div className={classes.mapContainer}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyBe1sqVJRAjtPF-rj7hvEYkiEJF0VTQeqo' }}
-        defaultCenter={coordinates}
-        center={coordinates}
-        defaultZoom={14}
-        margin={[50, 50, 50, 50]}
-        onChange={''}
-        onChildClick={''}
-      >
-      </GoogleMapReact>
-    </div>
+      <div className={classes.mapContainer}>
+          <MapContainer center = {coord} zoom = {14}>
+            <TileLayer
+              url = "https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=EnfLZu4HVEonP8CqxdMv"
+              attribution = '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+            />
+          </MapContainer>
+      </div>
   );
 };
 
